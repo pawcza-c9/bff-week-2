87f358027c27a42ddacf3edbcb12f4aed2ec661b
Hector Romo <info@hectorromo.com>
2021-12-03 13:18:10 +0100

    update

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,16 +1,5 @@
-import { useLoader, extend } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
-extend({ OrbitControls });
-
 const Model = () => {
-  const gltf = useLoader(GLTFLoader, "/models/car/scene.gltf");
-  return (
-    <>
-      <primitive position={[0, 0, 0]} object={gltf.scene} scale={1} />
-    </>
-  );
+  return <>Do stuff here...</>;
 };
 
 export default Model;
