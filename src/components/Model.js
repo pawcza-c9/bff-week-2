f917556a47f8aa7f26e1ca0da6c6c54527dea4c0
Paweł Czarniecki <pawel.czarniecki@cloudnine.se>
2022-02-04 14:55:13 +0100

    Doors now open hueee

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,16 +1,16 @@
 import { useRef } from 'react'
 import { useLoader, useFrame } from '@react-three/fiber'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const Model = () => {
 	const carRef = useRef();
 	
 	useFrame(({clock}) => {
 		const elapsedTime = clock.getElapsedTime();
-		carRef.current.rotation.y = -elapsedTime / 2;
+		// carRef.current.rotation.y = -elapsedTime / 2;
 	})
 	
-	const obj = useLoader(OBJLoader, './models/golf.obj');
+	const obj = useLoader(GLTFLoader, './models/golf.gltf');
 	return (
 			<primitive ref={carRef} object={obj} rotation={[0,0,0]} position={[-1, 0, 0]}/>
 	)};
