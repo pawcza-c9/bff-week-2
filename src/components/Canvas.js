362ea777bab34c956675fbf0a070fcbe64bab71a
Paweł Czarniecki <pawel.czarniecki@cloudnine.se>
2022-05-27 14:03:25 +0200

    Transform Controls

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -9,7 +9,7 @@ import Lights from "./examples/Lights";
 export default function CanvasBase() {
   return (
     <Canvas shadows camera={{ position: [0, 0, 100] }}>
-						<OrbitControls/>
+			<OrbitControls makeDefault/>
       <directionalLight position={[10, 10, 5]} intensity={1} />
       <directionalLight position={[-10, -10, -5]} intensity={1} />
       <Suspense fallback={null}>
