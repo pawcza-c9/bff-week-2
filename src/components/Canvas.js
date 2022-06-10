0e8be54b4b50eb37db70d218f03edaa3ad04ab76
Stavros Tsirlidis <43536754+StavrosTsirlidis24@users.noreply.github.com>
2022-06-10 14:06:17 +0200

    add new wheel

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,18 +3,20 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
 import Model from "./Model";
-import Golf from './Golf';
+import Golf from "./Golf";
+import Wheel from "./Wheel";
 import Lights from "./examples/Lights";
 
 export default function CanvasBase() {
   return (
     <Canvas shadows camera={{ position: [0, 0, 100] }}>
-						<OrbitControls/>
+      <OrbitControls />
       <directionalLight position={[10, 10, 5]} intensity={1} />
       <directionalLight position={[-10, -10, -5]} intensity={1} />
       <Suspense fallback={null}>
-								{/*<Stars/>*/}
-								<Golf/>
+        {/*<Stars/>*/}
+        <Golf />
+        <Wheel />
         {/*<Model />*/}
         <Lights />
       </Suspense>
