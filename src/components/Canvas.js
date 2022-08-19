af3328a3ca15b56b2223fb52b34c088f41da7130
Paweł Czarniecki <pawel.czarniecki@cloudnine.se>
2022-08-19 13:59:35 +0200

    Changed model & ADDED MOVING WHEELSgit add .git add .

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,20 +2,21 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
-import Model from "./Model";
 import Golf from './Golf';
+import FreeCar from './FreeCar';
 import Lights from "./examples/Lights";
 
 export default function CanvasBase() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 100] }}>
+    <Canvas shadows camera={{ position: [0, 0, 10] }}>
 			<OrbitControls makeDefault/>
       <directionalLight position={[10, 10, 5]} intensity={1} />
       <directionalLight position={[-10, -10, -5]} intensity={1} />
       <Suspense fallback={null}>
-								{/*<Stars/>*/}
-								<Golf/>
-        {/*<Model />*/}
+				{/*<Stars/>*/}
+				{/*<Golf/>*/}
+				{/*<Model />*/}
+				<FreeCar/>
         <Lights />
       </Suspense>
     </Canvas>
